bce77170d707081611b44d64c4e24e4112eaab79
mdeyell <mdeyell@ucsc.du>
2016-12-04 11:00:02 -0800

    moved more of the assignments code over

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -253,6 +253,82 @@ var app = function () {
         self.vue.is_adding_post = false;
     };
 
+    function get_assignments_function(start_idx, end_idx) {
+        var pp = {
+            start_idx: start_idx,
+            end_idx: end_idx
+        };
+
+        return get_assignments_url + "?" + $.param(pp);
+    }
+
+    self.get_assignments = function () {
+        $.getJSON(get_assignments_function(0, 20), function (data) {
+            self.vue.assignments = data.assignments;
+            self.vue.has_more = data.has_more;
+            self.vue.logged_in = data.logged_in;
+            sort_by_date(self.vue.assignments);
+            enumerate(self.vue.assignments);
+            self.vue.assignments.reverse();
+        })
+    };
+
+    self.get_more = function () {
+        var num_tracks = self.vue.assignments.length;
+        $.getJSON(get_assignments_url(num_tracks, num_tracks + 50), function (data) {
+            self.vue.has_more = data.has_more;
+            self.extend(self.vue.assignments, data.assignments);
+            enumerate(self.vue.assignments);
+        });
+    };
+
+    var sort_by_date = function (v) {
+        console.log("sort_by_date")
+        v.sort(function (a, b) {
+            if (a.due > b.due)
+                return -1;
+            else if (a.due < b.due)
+                return 1;
+            else
+                return 0;
+        });
+    }
+
+
+    self.add_assignment_button = function () {
+        // The button to add a track has been pressed.
+        self.vue.is_adding_assignment = !self.vue.is_adding_assignment;
+    };
+
+    self.add_assignment = function () {
+        // The submit button to add a track has been added.
+        $.post(add_assignment_url,
+            {
+                assignment_name: self.vue.assignment_name,
+                assignment_definition: self.vue.assignment_description,
+                due: self.vue.due,
+            },
+            function (data) {
+                $.web2py.enableElement($("#add_assignment"));
+                self.vue.assignments.unshift(data.track);
+                sort_by_date(self.vue.assignments);
+                enumerate(self.vue.assignments);
+                self.vue.assignments.reverse();
+            });
+
+    };
+
+
+    self.delete_assignment = function(track_idx) {
+        $.post(del_assignment_url,
+            { track_id: self.vue.assignments[track_idx].id },
+            function () {
+                self.vue.assignments.splice(track_idx, 1);
+                enumerate(self.vue.assignments);
+            }
+        )
+    };
+
 
 
     self.vue = new Vue({
@@ -277,7 +353,14 @@ var app = function () {
             user_email: null,
             post_search: "",
             page:"splash",
-            new_course_name: ""
+            new_course_name: "",
+            assignment_name: null,
+            assignment_description: null,
+            due: null,
+            is_adding_assignment: false,
+            assignments: [],
+            selected_idx: null,
+            selected_url: null,
         },
         methods: {
             load_more: self.load_more,
@@ -300,6 +383,7 @@ var app = function () {
 
     self.get_posts();
     self.get_courses();
+    self.get_assignments();
 
     $("#vue-div").show();
 
